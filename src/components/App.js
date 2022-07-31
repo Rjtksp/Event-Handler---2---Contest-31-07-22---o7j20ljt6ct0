c8fec3de0d703a572ae7387b2dcab3741084dcec
MindBlowingNoob <35030513+MindBlowingNoob@users.noreply.github.com>
2022-07-31 18:41:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 const App = () => {
 
-  const handleClick = (event) =>{
-    // use console.log
-
+  const handleClick1 = (event) =>{
+    console.log('Button id is:-button-a');
+  }
+  const handleClick2 = (event) =>{
+    console.log('Button id is:-button-b');
   }
 
   // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <button id="button-a">Button A</button>
-      <button id="button-b">Button B</button>
+      <button id="button-a" onClick={handleClick1}>Button A</button>
+      <button id="button-b" onClick={handleClick2}>Button B</button>
     </div>
   )
 }
